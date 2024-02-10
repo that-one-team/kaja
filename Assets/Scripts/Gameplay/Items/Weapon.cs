15554d757e4a0fa57ebebe7058813a0dccca2599
zsfer <seiferalbacete@gmail.com>
2024-02-10 13:33:38 +0800

    faster equip speed

--- Assets/Scripts/Gameplay/Items/Weapon.cs
+++ Assets/Scripts/Gameplay/Items/Weapon.cs
@@ -3,17 +3,18 @@ using UnityEngine;
 
 public class Weapon : Item
 {
+    [SerializeField] float _equipSpeed = 0.2f;
     bool _isReady = false;
 
     public void Equip()
     {
         transform.localPosition = Vector3.down;
-        transform.DOLocalMoveY(0, 0.5f).OnComplete(() => _isReady = true);
+        transform.DOLocalMoveY(0, _equipSpeed).OnComplete(() => _isReady = true);
     }
 
     public void Unequip()
     {
-        transform.DOLocalMoveY(Vector3.down.y, 0.5f).OnComplete(() => _isReady = false);
+        transform.DOLocalMoveY(Vector3.down.y, _equipSpeed).OnComplete(() => _isReady = false);
     }
 
     public void Shoot()

962c9bb67c5caee4bac19c366dbb981c8c83d4e8
zsfer <seiferalbacete@gmail.com>
2024-02-21 09:58:38 +0800

    blood decals

--- Assets/Scripts/Gameplay/VFX/GibVFX.cs
+++ Assets/Scripts/Gameplay/VFX/GibVFX.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class GibVFX : MonoBehaviour
 {
+    [SerializeField] bool _destroyAfterTime;
+
     AudioSource _source;
 
     private void Awake()
@@ -12,8 +14,9 @@ public class GibVFX : MonoBehaviour
 
     public virtual void DoGib(AudioClip clip)
     {
+        transform.eulerAngles = Vector3.up * Random.Range(0, 360);
         _source.spatialBlend = 1;
         _source.PlayOneShot(clip);
-        Destroy(gameObject, 5);
+        Destroy(gameObject, _destroyAfterTime ? 5 : 100000);
     }
 }

1168203e0b2c7d01cc9b81d633d09273454e27da
zsfer <seiferalbacete@gmail.com>
2024-03-19 22:04:02 +0800

    rocks

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.InputSystem;
 
 public class GameManager : SingletonBehaviour<GameManager>
--- Assets/Scripts/Core/GameSettings.cs
+++ Assets/Scripts/Core/GameSettings.cs
@@ -1,17 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
+using UnityEngine.Audio;
 
-public class GameSettings : MonoBehaviour
+public class GameSettings : SingletonBehaviour<GameSettings>
 {
+    public float MouseSensitivity { get; set; }
+    public float AudioVolume { get; private set; }
 
-    void Start()
+    [SerializeField] AudioMixer _mixer;
+    float _prevVolume;
+
+    private void Start()
     {
+        MouseSensitivity = PlayerPrefs.GetFloat("mouse_sensitivity", 3);
+        AudioVolume = PlayerPrefs.GetFloat("audio_volume", 3);
+    }
 
+    public void ChangeVolume(float vol)
+    {
+        AudioVolume = vol;
+        PlayerPrefs.SetFloat("game_volume", AudioVolume);
+    }
+
+    public void ChangeSens(float sens)
+    {
+        MouseSensitivity = sens;
+        PlayerPrefs.SetFloat("mouse_sensitivity", AudioVolume);
     }
 
     void Update()
     {
+        if (!Mathf.Approximately(AudioVolume, _prevVolume))
+        {
+            _mixer.SetFloat("Master", AudioVolume);
+        }
 
+        _prevVolume = AudioVolume;
     }
 }
